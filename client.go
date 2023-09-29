0083cc24f1031a81e5d9252ddfd495f22d7d0f32
jigmetnamgyal <tashinamgyaljigme@gmail.com>
2023-09-29 15:20:31 +0400

    Feat(): get usr input for a number, connects a network with dial func

--- client.go
+++ client.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"os"
 )
 
@@ -9,5 +10,24 @@ func main() {
 	if len(os.Args) != 3 {
 		fmt.Println("Usage: client <server_address> <server_port>")
 		os.Exit(1)
+	}
+
+	serverAddr := os.Args[1]
+	serverPort := os.Args[2]
+
+	for {
+		fmt.Println("Enter a number: ")
+		var input string
+		_, err := fmt.Scanln(&input)
+		if err != nil {
+			fmt.Println("Error Scanning: ", err)
+			os.Exit(1)
+		}
+
+		conn, err := net.Dial("tcp", serverAddr+":"+serverPort)
+		if err != nil {
+			fmt.Println("Error connecting to the server:", err)
+			continue
+		}
 	}
 }
